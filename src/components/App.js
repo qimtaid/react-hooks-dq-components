85bf3d253e50f47d29294f2b6fcaa858eed51249
Dan Koskei <qimtaid@gmail.com>
2024-04-24 23:16:42 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import TopMenu from "../components/TopMenu";
 import SideMenu from "../components/SideMenu";
-import AlbumCard from "../components/AlbumCard";
-import AlbumList from "../components/AlbumList";
-import GenrePicker from "../components/GenrePicker";
-import Logo from "../components/Logo";
 import MainContent from "../components/MainContent";
-import MenuLinks from "../components/MenuLinks";
-import Search from "../components/Search";
+
 
 
 import "../index.css";
@@ -20,14 +15,11 @@ function App() {
       
   <div className="app-container">
     <MainContent/>
-     <Logo/>
+     
      <TopMenu />
     <SideMenu/>
-     <MenuLinks/>
-    <Search/>
-    <AlbumCard/>
-  <AlbumList/>
-  <GenrePicker/>
+    
+
  
   
 
